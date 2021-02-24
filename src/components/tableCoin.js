ce2380f406e12f7ca7396ad66e4752e859a8b269
Olcay Kaplan <olcay.kaplan@datassist.com.tr>
2021-02-24 11:18:47 +0300

    tablo sıralama

--- src/components/tableCoin.js
+++ src/components/tableCoin.js
@@ -25,36 +25,42 @@ const columns = [
         title: 'en düşük tarih',
         dataIndex: 'atl_date',
         key: 'atl_date',
+        sorter: (a, b) => moment(a.atl_date).unix() - moment(b.atl_date).unix(),
         render: alt_date =>  <span>{moment(alt_date).format(dateFormat)}</span>
     },
     {
         title: 'en düşük değeri',
         key: 'atl',
         dataIndex: 'atl',
+        sorter: (a, b) => a.atl - b.atl,
         render: atl =>  <span>{currentFixed(atl)  + "$"}</span>
     },
     {
         title: 'en yüksek tarih',
         dataIndex: 'ath_date',
         key: 'ath_date',
+        sorter: (a, b) => moment(a.ath_date).unix() - moment(b.ath_date).unix(),
         render: alh_date =>  <span>{moment(alh_date).format(dateFormat)}</span>
     },
     {
         title: 'en yüksek değeri',
         key: 'ath',
         dataIndex: 'ath',
+        sorter: (a, b) => a.ath - b.ath,
         render: ath =>  <span>{currentFixed(ath)  + "$"}</span>
     },
     {
         title: 'Güncel fiyat',
         key: 'current_price',
         dataIndex: 'current_price' ,
+        sorter: (a, b) => a.current_price - b.current_price,
         render: current_price =>  <span style={{color:"#16c784", fontWeight:"bold"}}>{currentFixed(current_price) + " $"}</span>
     },
     {
         title: 'Değişim %(en düşük değer ile güncel fiyat)',
         key: 'atl_change_percentage',
         dataIndex: 'atl_change_percentage' ,
+        sorter: (a, b) => a.atl_change_percentage - b.atl_change_percentage,
         render: atl_change_percentage =>  <span>{" % "+currentFixed(atl_change_percentage,0) }</span>
     },
 ];
@@ -70,9 +76,8 @@ const TableCoin = ({marketData}) => {
         <>
             <Input
                 style={{ border: "3px solid  #16c784", margin: "0 0 10px 0" }}
-                placeholder="Search by id"
+                placeholder="Search by id (bitcoin) & symbol (btc) "
                 onChange={(e) => searchHandle(e.target.value)}
-
             />
         <Table columns={columns} dataSource={filtredData === null ? marketData : filtredData} />
         {/*<Table columns={columns} dataSource={marketData} />*/}
